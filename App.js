aff31dc802f6e86ee8638e7e227b3a67d8ff06ee
Bob Pakhriev <boburpakhriev@gmail.com>
2023-02-25 21:03:27 +0900

    Fix NaN output

--- App.js
+++ App.js
@@ -4,7 +4,13 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 export default function App() {
   const [counts, setCounts] = useState(new Array(11).fill(0));
 
-  const test_get_random = () => {
+
+
+  function get_1_or_0() {
+    return Math.floor(Math.random() * 2);
+  }
+
+function test_get_random () {
     const counts = new Array(11).fill(0);
     for (let i = 0; i < 1000; i++) {
       const r = get_random(10);
@@ -12,13 +18,42 @@ export default function App() {
     }
     setCounts(counts);
   }
+  //
+  // function test_get_random() {
+  //   let frequency = {};
+  //   for (let i = 0; i < 100000; i++) {
+  //     let randomNumber = get_random(10);
+  //     if (frequency[randomNumber] === undefined) {
+  //       frequency[randomNumber] = 0;
+  //     }
+  //     frequency[randomNumber]++;
+  //   }
+  //   console.log(frequency);
+  //   // setCounts(frequency);
+  // }
 
+  console.log(counts)
+
+  function get_random(n) {
+    if (n === 0) {
+      return 0;
+    }
+    let bitCount = Math.floor(Math.log2(n)) + 1;
+    let randomNumber = 0;
+    for (let i = 0; i < bitCount; i++) {
+      let bit = get_1_or_0();
+      randomNumber |= (bit << i);
+    }
+    if (randomNumber > n) {
+      randomNumber = get_random(n);
+    }
+    return randomNumber;
+  }
   return (
       <View style={styles.container}>
         <Button title="Test get_random" onPress={test_get_random} />
         <View style={styles.counts}>
-          {counts.slice(0,10).map((count, i) => (
-              console.log(count),
+          {counts.map((count, i) => (
               <Text key={i} style={styles.count}>{count}</Text>
           ))}
         </View>
@@ -26,20 +61,7 @@ export default function App() {
   );
 }
 
-function get_1_or_0() {
-  return Math.floor(Math.random() * 2);
-}
 
-function get_random(n) {
-  let result = 0;
-  let count = 0;
-  while (n > 0) {
-    result = (result << 1) | get_1_or_0();
-    n = Math.floor(n / 2);
-    count++;
-  }
-  return result;
-}
 
 const styles = StyleSheet.create({
   container: {
@@ -50,10 +72,12 @@ const styles = StyleSheet.create({
   counts: {
     flexDirection: 'row',
     flexWrap: 'wrap',
+    alignItems:'center',
+    justifyContent: 'space-evenly',
     marginTop: 20,
   },
   count: {
-    marginHorizontal: 10,
+    marginHorizontal: 20,
     fontSize: 20,
     fontWeight: 'bold',
   },
