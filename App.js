a822273971a0b288edd17e8e98f649bcfdd1e33b
Bob Pakhriev <boburpakhriev@gmail.com>
2023-02-26 10:47:15 +0900

    Add Chart visualization

--- App.js
+++ App.js
@@ -1,84 +1,106 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Dimensions,
+  SafeAreaView,
+  ScrollView,
+} from "react-native";
+import { BarChart } from "react-native-chart-kit";
 
 export default function App() {
-  const [counts, setCounts] = useState(new Array(11).fill(0));
-
-
-
+  const [count, setCount] = useState([]);
+  const deviceWidth = Dimensions.get("screen").width;
+  console.log(deviceWidth);
   function get_1_or_0() {
     return Math.floor(Math.random() * 2);
   }
 
-function test_get_random () {
-    const counts = new Array(11).fill(0);
-    for (let i = 0; i < 1000; i++) {
-      const r = get_random(10);
-      counts[r]++;
-    }
-    setCounts(counts);
-  }
-  //
-  // function test_get_random() {
-  //   let frequency = {};
-  //   for (let i = 0; i < 100000; i++) {
-  //     let randomNumber = get_random(10);
-  //     if (frequency[randomNumber] === undefined) {
-  //       frequency[randomNumber] = 0;
-  //     }
-  //     frequency[randomNumber]++;
-  //   }
-  //   console.log(frequency);
-  //   // setCounts(frequency);
-  // }
-
-  console.log(counts)
-
   function get_random(n) {
-    if (n === 0) {
-      return 0;
+    let bitsNeeded = Math.ceil(Math.log2(n + 1));
+    let randomNum = 0;
+    let bitIndex = 0;
+    while (bitIndex < bitsNeeded) {
+      randomNum <<= 1;
+      randomNum |= get_1_or_0();
+      bitIndex++;
     }
-    let bitCount = Math.floor(Math.log2(n)) + 1;
-    let randomNumber = 0;
-    for (let i = 0; i < bitCount; i++) {
-      let bit = get_1_or_0();
-      randomNumber |= (bit << i);
+    if (randomNum <= n) {
+      return randomNum;
     }
-    if (randomNumber > n) {
-      randomNumber = get_random(n);
-    }
-    return randomNumber;
+    return get_random(n);
   }
+
+  function test_get_random() {
+    let counts = Array(11).fill(0);
+    for (let i = 0; i < 100000; i++) {
+      let rand = get_random(10);
+      counts[rand]++;
+    }
+    setCount(counts);
+  }
+
+  let data = {
+    labels: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+    datasets: [{ data: count }],
+  };
+
   return (
+    <>
+      <SafeAreaView></SafeAreaView>
       <View style={styles.container}>
-        <Button title="Test get_random" onPress={test_get_random} />
-        <View style={styles.counts}>
-          {counts.map((count, i) => (
-              <Text key={i} style={styles.count}>{count}</Text>
-          ))}
-        </View>
+        <BarChart
+          data={data}
+          width={350}
+          height={350}
+          chartConfig={{
+            backgroundColor: "#e26a00",
+            backgroundGradientFrom: "#fb8c00",
+            backgroundGradientTo: "#ffa726",
+            decimalPlaces: 0,
+            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            labelColor: (opacity = 1) => `#000`,
+            style: { borderRadius: 16 },
+          }}
+          style={styles.chart}
+        />
+        <Button title="Get Random" onPress={test_get_random} />
       </View>
+      <View style={styles.counts}>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          {count.map((count, index) => (
+            <Text style={styles.count} key={index}>
+              {index + 1} : {count}
+            </Text>
+          ))}
+        </ScrollView>
+      </View>
+    </>
   );
 }
-
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  chart: {
+    marginVertical: 8,
+    borderRadius: 16,
   },
   counts: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    alignItems:'center',
-    justifyContent: 'space-evenly',
-    marginTop: 20,
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-between",
+    backgroundColor: "#e26a00",
+    paddingVertical: 20,
   },
   count: {
-    marginHorizontal: 20,
-    fontSize: 20,
-    fontWeight: 'bold',
+    color: "white",
+    fontSize: 16,
+    marginVertical: 10,
   },
 });
